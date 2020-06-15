618ebb73fa0a972d64d594a29e534a0ab1a3d878
DanielOlopade <66929969+DanielOlopade@users.noreply.github.com>
2020-06-14 21:57:21 -0500

    Update Day1.py
    Update

--- Day1.py
+++ Day1.py
@@ -1,17 +1,22 @@
-var1 = int(input("Put a number..."))
-var2 = int(input("Put a number..."))
-var3 = input("Put a sign...")
-if (var3 == "+"):
-    print(var1 + var2)
-if var3 == "-":
-    print(var1 - var2)
-if var3 == "/":
-    ans = (var1 / var2)
-    ans = round(ans, 2)
-    print(ans)
-if var3 == "*":
-    dan = (var1 * var2)
-    print(dan)
-
+def run():
+    var1 = int(input("Put a number..."))
+    var2 = int(input("Put a number..."))
+    var3 = input("Put a sign...")
+    if (var3 == "+"):
+        print(var1 + var2)
+    if var3 == "-":
+        print(var1 - var2)
+    if var3 == "/":
+        ans = (var1 / var2)
+        ans = round(ans, 2)
+        print(ans)
+    if var3 == "*":
+        dan = (var1 * var2)
+        print(dan)
+run()
+if input() == "n":
+    quit()
+else:
+    run()
     
 
